12369f7f1bf3004dd970a528ab6e9d7b09d4cfa0
Alex J Costa <alexjcosta01@gmail.com>
2017-05-09 14:55:13 -0300

    Creating ValorProcesso in expressions2 and Updating attributes in DecProcesso.

--- plp/src/lf3/plp/expressions2/expression/ValorProcesso.java
+++ plp/src/lf3/plp/expressions2/expression/ValorProcesso.java
@@ -1,11 +1,9 @@
-package lf3.plp.functional3.declaration;
+package lf3.plp.expressions2.expression;
 
+import java.util.List;
 import java.util.Map;
 
 import lf3.plp.expressions1.util.Tipo;
-import lf3.plp.expressions2.expression.Id;
-import lf3.plp.expressions2.expression.Valor;
-import lf3.plp.expressions2.expression.ValorProcesso;
 import lf3.plp.expressions2.memory.AmbienteCompilacao;
 import lf3.plp.expressions2.memory.AmbienteExecucao;
 import lf3.plp.expressions2.memory.VariavelJaDeclaradaException;
@@ -14,59 +12,55 @@ import lf3.plp.functional1.declaration.DeclaracaoFuncional;
 import lf3.plp.functional2.expression.ExpDeclaracao;
 import lf3.plp.functional2.expression.ValorFuncao;
 
-public class DecProcesso implements DeclaracaoFuncional {
-	private Id id;
-	private ValorProcesso valorProcesso;
+public class ValorProcesso implements DeclaracaoFuncional{
 	
-
-	public DecProcesso(Id id, ValorProcesso valorProcesso) {
+	private Id id;
+	private List<ExpDeclaracao> listExpDeclaracao;
+	
+	public ValorProcesso(Id id, List<ExpDeclaracao> listExpDeclaracao) {
 		this.id = id;
-		this.valorProcesso = valorProcesso;
+		this.listExpDeclaracao = listExpDeclaracao;
 	}
-
+	
 	@Override
 	public boolean checaTipo(AmbienteCompilacao ambiente)
 			throws VariavelNaoDeclaradaException, VariavelJaDeclaradaException {
-		return (valorProcesso.checaTipo(ambiente));
+		// TODO Auto-generated method stub
+		return false;
 	}
-
 	@Override
 	public void elabora(AmbienteExecucao amb, Map<Id, Valor> declaracoes, Map<Id, ValorFuncao> declaracoesFuncoes)
 			throws VariavelJaDeclaradaException {
 		// TODO Auto-generated method stub
 		
 	}
-
 	@Override
 	public void elabora(AmbienteCompilacao amb, Map<Id, Tipo> tipos) throws VariavelJaDeclaradaException {
 		// TODO Auto-generated method stub
 		
 	}
-
 	@Override
 	public void incluir(AmbienteExecucao amb, Map<Id, Valor> declaracoes, Map<Id, ValorFuncao> declaracoesFuncoes)
 			throws VariavelJaDeclaradaException {
 		// TODO Auto-generated method stub
 		
 	}
-
 	@Override
 	public void incluir(AmbienteCompilacao amb, Map<Id, Tipo> tipos, boolean incluirCuringa)
 			throws VariavelJaDeclaradaException {
 		// TODO Auto-generated method stub
 		
 	}
-
 	@Override
 	public void reduzir(AmbienteExecucao amb) {
 		// TODO Auto-generated method stub
 		
 	}
-
 	@Override
 	public DeclaracaoFuncional clone() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 
 }
