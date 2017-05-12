ec71a78c9eb85059828556a9364da610058a4574
Victor Oliveira <victorlaertedoliveira@gmail.com>
2017-05-11 23:52:29 -0300

    Adicionando ao ambiente de execução métodos para enviar e recuperar mensagens. Criando map e lógica de mensagens no ContextoExecucao.

--- plp/src/lf3/plp/expressions2/memory/AmbienteExecucao.java
+++ plp/src/lf3/plp/expressions2/memory/AmbienteExecucao.java
@@ -2,9 +2,12 @@ package lf3.plp.expressions2.memory;
 
 import lf3.plp.expressions2.expression.Valor;
 
-
 public interface AmbienteExecucao extends Ambiente<Valor> {
 
 	public AmbienteExecucao clone();
+
+	public void putMessageInQueue(String processId, String message) throws InterruptedException;
+
+	public String takeMessageFromQueue(String processId, int timeoutInSeconds);
 
 }
--- plp/src/lf3/plp/expressions2/memory/ContextoExecucao.java
+++ plp/src/lf3/plp/expressions2/memory/ContextoExecucao.java
@@ -1,33 +1,73 @@
 package lf3.plp.expressions2.memory;
 
 import java.util.HashMap;
-import java.util.Stack;
+import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Stack;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 import lf3.plp.expressions2.expression.Id;
 import lf3.plp.expressions2.expression.Valor;
 import lf3.plp.functional1.memory.ContextoExecucaoFuncional;
 
+public class ContextoExecucao extends Contexto<Valor> implements AmbienteExecucao {
 
-public class ContextoExecucao extends Contexto<Valor>
-        implements AmbienteExecucao {
+	public static final int DEFAULT_QUEUE_SIZE = 1000;
+	public Map<String, BlockingQueue<String>> map = new HashMap<>();
 
+	@Override
 	public ContextoExecucao clone() {
+
 		ContextoExecucaoFuncional retorno = new ContextoExecucaoFuncional();
-		
+
 		Stack<HashMap<Id, Valor>> novaPilha = new Stack<HashMap<Id, Valor>>();
-		
+
 		HashMap<Id, Valor> novoMap = new HashMap<Id, Valor>();
 		novaPilha.add(novoMap);
 
-		for (HashMap<Id, Valor> map : this.pilha){
-			for(Entry<Id, Valor> entry : map.entrySet()){
+		for (HashMap<Id, Valor> map : this.pilha) {
+			for (Entry<Id, Valor> entry : map.entrySet()) {
 				novoMap.put(entry.getKey(), entry.getValue());
 			}
 		}
-		
+
 		retorno.setPilha(novaPilha);
-		
+
 		return retorno;
+	}
+
+	@Override
+	public void putMessageInQueue(String processId, String message) throws InterruptedException {
+
+		if (map.containsKey(processId)) {
+			BlockingQueue<String> blockingQueue = map.get(processId);
+			blockingQueue.put(processId);
+
+		} else {
+			BlockingQueue<String> blockingQueue = new ArrayBlockingQueue<>(DEFAULT_QUEUE_SIZE);
+			map.put(processId, blockingQueue);
+		}
+	}
+
+	@Override
+	public String takeMessageFromQueue(String processId, int timeoutInSeconds) {
+
+		BlockingQueue<String> blockingQueue = map.get(processId);
+
+		String message = "";
+
+		if (blockingQueue == null) {
+			//TODO: Lançar Exception
+		} else {
+			try {
+				message = blockingQueue.poll(timeoutInSeconds, TimeUnit.SECONDS);
+			} catch (InterruptedException e) {
+				//TODO: decidir o comportamento
+			}
+		}
+
+		return message;
 	}
 }
